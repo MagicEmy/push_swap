178e11b428e0fa8fb89b42cc67c7085f216ac152
Magic <emanuela.licameli@gmail.com>
2022-08-17 16:56:19 +0200

    Add files via upload

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: emlicame <emlicame@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/06 13:56:30 by emlicame      #+#    #+#                 */
-/*   Updated: 2022/08/17 16:37:09 by emlicame      ########   odam.nl         */
+/*   Updated: 2022/08/17 16:54:15 by emlicame      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@ void	error_and_free(t_list *stack_a, t_list *stack_b)
 	if (stack_b)
 		list_clear(&stack_b);
 	write(2, "Error\n", 6);
+}
+void	end_free(t_list *stack_a, t_list *stack_b)
+{
+	if (stack_a)
+		list_clear(&stack_a);
+	if (stack_b)
+		list_clear(&stack_b);
 }
 
 void	sorting_tree(t_list **stack_a, t_list **stack_b)
@@ -34,19 +41,24 @@ void	sorting_tree(t_list **stack_a, t_list **stack_b)
 	else if (list_size(*stack_a) > 5)
 		sort_big(stack_a, stack_b);
 }
-
+void	checkleaks()
+{
+	system("leaks push_swap");
+}
 int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
-
+	
 	stack_a = NULL;
 	stack_b = NULL;
+	atexit(checkleaks);
 	if (argc < 2)
 		return (0);
 	if (!check_input(&stack_a, argv))
 		return (0);
 	sorting_tree(&stack_a, &stack_b);
+	end_free(stack_a, stack_b);
 //	return (system ("leaks -q push_swap"), 0);
 	return (0);
 }
